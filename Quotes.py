a612f5fa93174ebc4efc6e8902b77d788ab770a9
metehan <metehan@MetehanUbuntu>
2016-10-30 08:55:40 +0200

    created class for Quotes and created 2 sample objects to insert

---
+++ Quotes.py
@@ -0,0 +1,8 @@
+class Quote:
+    def __init__(self, quotetext, author_id, book_id):
+        self.quotetext = quotetext
+        self.author_id = author_id
+        self.book_id = book_id
+
+quote1 = Quote("you can't get away from yourself by moving from one place to another.", 1, 1)
+quote2 = Quote("All right, then, I'll go to hell.", 2, 2)
