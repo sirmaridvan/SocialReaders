c1e42e3a41bbcc16680b928ddf59c43916c0b135
Ömer <ogreko@itu.edu.tr>
2016-11-03 00:23:55 +0200

    added select delete and uptade functions to the news class

--- News.py
+++ News.py
@@ -9,3 +9,23 @@ def insert_news(cursor,news):
                 %s, %s, %s
                 )"""
     cursor.execute(statement,(news.newstext,news.newsdate,news.newsheadline));
+
+def select_newsdate(cursor):
+        statement = """SELECT * FROM NEWS ORDER BY NEWSDATE ASC"""
+        cursor.execute(statement)
+        news = cursor.fetchall()
+        return news
+    
+def select_newsheadline(cursor):
+        statement = """SELECT * FROM NEWS ORDER BY NEWSHEADLINE ASC"""
+        cursor.execute(statement)
+        news = cursor.fetchall()
+        return news
+        
+def delete_news(dsn,newsdate):
+        statement = """ DELETE FROM NEWS WHERE NEWSDATE = %s"""
+        cursor.execute(statement,(newsdate))
+        
+def update_news(cursor,newsheadline):
+        statement = """ UPDATE NEWS SET NEWSTEXT = %s NEWSDATE = %s NEWSHEADLINE = %s WHERE NEWSHEADLINE = %s """
+        cursor.execute(statement)
