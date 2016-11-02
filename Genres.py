2f3422aff409f66b12a2f801eff99966de73b4b4
metehan <metehan@MetehanUbuntu>
2016-11-02 11:36:00 +0200

    moved genres functions to genres class

--- Genres.py
+++ Genres.py
@@ -1,6 +1,11 @@
 class Genre:
-    def __init__(self, genre_name):
+    def __init__(self, genreid, genre_name):
+        self.genreid = genreid
         self.genre_name = genre_name
 
-genre1 = Genre("Novel")
-genre2 = Genre("Satire")
+def insert_genre(cursor, genre):
+        statement = """INSERT INTO GENRES (GENRENAME) VALUES (
+                %s
+                )"""
+        cursor.execute(statement,(genre.genre_name))
+
