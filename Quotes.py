83ceef15ceda0930c3a61e4d0dfe53063dd0260c
metehan <metehan@MetehanUbuntu>
2016-11-02 11:36:09 +0200

    moved quotes functions to quotes class

--- Quotes.py
+++ Quotes.py
@@ -1,8 +1,12 @@
 class Quote:
-    def __init__(self, quotetext, author_id, book_id):
+    def __init__(self, quoteid, quotetext, author_id, book_id):
+        self.quoteid = quoteid
         self.quotetext = quotetext
         self.author_id = author_id
         self.book_id = book_id
 
-quote1 = Quote("you can't get away from yourself by moving from one place to another.", 1, 1)
-quote2 = Quote("All right, then, I'll go to hell.", 2, 2)
+def insert_quote(cursor, quote):
+        statement = """INSERT INTO QUOTES (QUOTETEXT,AUTHORID,BOOKID) VALUES (
+                %s, %s, %s
+                )"""
+        cursor.execute(statement,(quote.quotetext, quote.author_id, quote.book_id))
