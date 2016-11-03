725382fb12181a493b3056cf2dabe07a97b8ef03
metehan <metehan@MetehanUbuntu>
2016-11-03 16:01:42 +0200

    created select, insert, delete and update methods for genres class

--- Genres.py
+++ Genres.py
@@ -1,11 +1,45 @@
+import psycopg2 as dbapi2
+
 class Genre:
-    def __init__(self, genreid, genre_name):
-        self.genreid = genreid
-        self.genre_name = genre_name
+    def __init__(self,id,name):
+        self.id = id
+        self.name= name
 
-def insert_genre(cursor, genre):
-        statement = """INSERT INTO GENRES (GENRENAME) VALUES (
-                %s
-                )"""
-        cursor.execute(statement,(genre.genre_name))
+def insert_genre(dsn,genre):
+    with dbapi2.connect(dsn) as connection:
+        cursor = connection.cursor()
+        statement = """ INSERT INTO GENRES (NAME) VALUES (%s)"""
+        cursor.execute(statement,(genre.name,))
+        cursor.close()
 
+def selectGenre(dsn):
+    with dbapi2.connect(dsn) as connection:
+        cursor = connection.cursor()
+        statement = """SELECT * FROM GENRES ORDER BY ID ASC"""
+        cursor.execute(statement)
+        genres = cursor.fetchall()
+        return genres
+        cursor.close()
+
+def selectGenrebyName(dsn,name):
+    with dbapi2.connect(dsn) as connection:
+        cursor = connection.cursor()
+        statement = """SELECT * FROM GENRES WHERE NAME = %s"""
+        cursor.execute(statement,(name))
+        genres = cursor.fetchall()
+        return genres
+        cursor.close()
+
+def deleteGenre(dsn,id):
+    with dbapi2.connect(dsn) as connection:
+        cursor = connection.cursor()
+        statement = """ DELETE FROM GENRES WHERE ID = %s"""
+        cursor.execute(statement,(id))
+        cursor.close()
+
+def updateGenre(dsn,id,newgenre):
+    with dbapi2.connect(dsn) as connection:
+        cursor = connection.cursor()
+        statement = """ UPDATE GENRES SET NAME = %s WHERE ID = %s"""
+        cursor.execute(statement,(newgenre.name,id))
+        cursor.close()
