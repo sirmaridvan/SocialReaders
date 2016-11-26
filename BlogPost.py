6bfb2d982d6d8cc42733f19d731cb70a4e9f132a
sirmar <sirmar@itu.edu.tr>
2016-11-26 19:35:38 +0200

    +BLOGS tablosundaki USERID, SITEUSER tablosundaki USERID niteliğine referans gösterildi. +Bununla ilgili gerekli 'statement'lar düzenlendi.

--- BlogPost.py
+++ BlogPost.py
@@ -1,33 +1,26 @@
 import random
 
 class BlogPost:
-    def __init__(self, id, userName, date, header, text):
+    def __init__(self, id, userId, date, header, text):
         self.id = id
-        self.userName = userName
+        self.userId = userId
         self.date=date
         self.header = header
         self.text = text
 
-def createRandomUserName():
-    ALPHABET = "0123456789ABCDEFGHJIKLMNOPQRSTUVQXYZabcdefghijklmnopqrstuvwxyz"
-    chars=[]
-    for i in range(3):
-        chars.append(random.choice(ALPHABET))
-    return chars
-
 def insert_blogPost(cursor,post):
-    statement = """INSERT INTO BLOGS (USERNAME,DATE,HEADER,TEXT) VALUES (
+    statement = """INSERT INTO BLOGS (USERID,DATE,HEADER,TEXT) VALUES (
                 %s, %s, %s, %s
                 )"""
-    cursor.execute(statement,(post.userName,post.date,post.header,post.text));
+    cursor.execute(statement,(post.userId,post.date,post.header,post.text));
 
 def getPost(cursor,id):
-    statement = """SELECT ID,USERNAME,DATE,HEADER,TEXT FROM BLOGS WHERE (ID = %(id)s)"""
+    statement = """SELECT ID,USERNAME,DATE,HEADER,TEXT FROM BLOGS,SITEUSER WHERE (USERID=SITEUSER.USERID) AND (ID = %(id)s)"""
     cursor.execute(statement, {'id':id})
 
 def getAllPosts(cursor):
-    statement = """SELECT ID,USERNAME,DATE,HEADER,TEXT FROM BLOGS"""
+    statement = """SELECT ID,USERNAME,DATE,HEADER,TEXT FROM BLOGS,SITEUSER WHERE (BLOGS.USERID=SITEUSER.USERID)"""
     cursor.execute(statement)
-def deletePost(cursor,name):
-    statement = """DELETE FROM BLOGS WHERE (USERNAME = %(name)s)"""
-    cursor.execute(statement,{'name':name})
+def deletePost(cursor,id):
+     statement = """DELETE FROM BLOGS WHERE (ID = %(id)s)"""
+     cursor.execute(statement,{'id':id})
